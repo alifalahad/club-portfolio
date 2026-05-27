70a4d3fe076d13cd4f14db5b65980c9dc353089e
Alif Al Ahad <alifalahadxi@gmail.com>
2026-05-27 16:00:00 +0600

    Week 7: EVENTS CRUD

---
+++ Controllers/EventsController.cs
@@ -0,0 +1,103 @@
+using EventHorizon.Data;
+using EventHorizon.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventHorizon.Controllers
+{
+    public class EventsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public EventsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Events (Read Events)
+        public async Task<IActionResult> Index()
+        {
+            var events = await _context.Events.ToListAsync();
+            return View(events);
+        }
+
+        // GET: Events/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Events/Create
+        [HttpPost]
+        public async Task<IActionResult> Create(Event newEvent)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Events.Add(newEvent);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(newEvent);
+        }
+
+        // GET: Events/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var eventToEdit = await _context.Events.FindAsync(id);
+            if (eventToEdit == null) return NotFound();
+
+            return View(eventToEdit);
+        }
+
+        // POST: Events/Edit/5
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Event editedEvent)
+        {
+            if (id != editedEvent.Id) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(editedEvent);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.Events.Any(e => e.Id == editedEvent.Id))
+                        return NotFound();
+                    else
+                        throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(editedEvent);
+        }
+
+        // GET: Events/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var eventToDelete = await _context.Events.FirstOrDefaultAsync(m => m.Id == id);
+            if (eventToDelete == null) return NotFound();
+
+            return View(eventToDelete);
+        }
+
+        // POST: Events/Delete/5
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var eventToDelete = await _context.Events.FindAsync(id);
+            if (eventToDelete != null)
+            {
+                _context.Events.Remove(eventToDelete);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+using EventHorizon.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventHorizon.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Event> Events { get; set; }
+    }
+}
